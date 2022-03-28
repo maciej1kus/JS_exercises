93c78d0398612d3c1f7a3e9626b987cb5dacfdab
maciej1kus <maciej1kus@gmail.com>
2022-03-28 15:55:34 +0200

    exercise 1 solved

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 // Exercise 1
+console.log("The cake was delicious")
+
+String.prototype.setAtTheEnd = function(word) {
+    let text = `${this.replace(word, "")} ${word}`;
+
+    return text.split("  ").join(" ");
+}
+
+console.log("The cake was delicious".setAtTheEnd("cake"));
